6cdf9307515f34fc5b7604d4e04c232d38f6b92b
jungHyeonS <fbtls25@naver.com>
2022-10-08 15:59:21 +0900

    react-native : AppLoading

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { Image, Text } from 'react-native';
+import * as Font from "expo-font"
+import {Ionicons} from "@expo/vector-icons"
+import { Asset, useAssets } from 'expo-asset';
+
+
+// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font))
+
+// const loadImages = (images) => images.map(image => {
+//   if(typeof image == "string"){
+//     Image.prefetch(image)
+//   }else{
+//     Asset.loadAsync(image)
+//   }
+// })
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [assets] = useAssets([require("./images.png")])
+  const [loaded] = Font.useFonts(Ionicons.font);
+  // const [ready,setReady] = useState(false);
+
+  // const onFinish = () => setReady(true);
+  // const startLoading = async() => {
+  //   const fonts = loadFonts([Ionicons.font])
+  //   const assets = loadImages([require("./images.png"),"http://cogly.net/rainbow.png"])
+  //   await Promise.all([...fonts,...assets])
+  // } 
+  if(!assets || !loaded){
+    return (
+      <AppLoading/>
+      // <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.log('abc')}/>
+     );
+  }
+  return <Text>We are done l1oading</Text>;
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
