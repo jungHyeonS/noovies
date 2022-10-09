a7d5e21581ac0686ff4a73a775845e41d25c8a40
jungHyeonS <fbtls25@naver.com>
2022-10-09 13:41:27 +0900

    react-native : tab navigation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading';
-import { Image, Text } from 'react-native';
+import { Image, Text,View } from 'react-native';
 import * as Font from "expo-font"
 import {Ionicons} from "@expo/vector-icons"
 import { Asset, useAssets } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
 
 
 // const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font))
@@ -20,21 +22,16 @@ export default function App() {
 
   const [assets] = useAssets([require("./images.png")])
   const [loaded] = Font.useFonts(Ionicons.font);
-  // const [ready,setReady] = useState(false);
-
-  // const onFinish = () => setReady(true);
-  // const startLoading = async() => {
-  //   const fonts = loadFonts([Ionicons.font])
-  //   const assets = loadImages([require("./images.png"),"http://cogly.net/rainbow.png"])
-  //   await Promise.all([...fonts,...assets])
-  // } 
   if(!assets || !loaded){
     return (
       <AppLoading/>
-      // <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.log('abc')}/>
      );
   }
-  return <Text>We are done l1oading</Text>;
+  return (
+     <NavigationContainer>
+      <Tabs/>
+    </NavigationContainer>
+  );
   
 }
 
