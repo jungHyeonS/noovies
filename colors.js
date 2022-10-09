980b0603262bc93eb9a2c6b322d2bc82eb7938d7
jungHyeonS <fbtls25@naver.com>
2022-10-09 14:12:45 +0900

    react-native : dark mode

---
+++ colors.js
@@ -0,0 +1,5 @@
+export const YELLOW_COLOR = "#ffa801";
+export const BLACK_COLOR = "#1e272e";
+export const GRAY_COLOR = "#d2dae2";
+export const DARK_GRAY_COLOR = "#808e9b";
+export const WHITE_COLOR = "#FFFFFF";
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,30 +3,50 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-import { Text,View } from "react-native";
+import { Text,View ,useColorScheme} from "react-native";
+import { BLACK_COLOR, DARK_GRAY_COLOR, GRAY_COLOR, WHITE_COLOR, YELLOW_COLOR } from "../colors";
+
 const Tab = createBottomTabNavigator();
 
 
-const Tabs = () => (
-    <Tab.Navigator >
+const Tabs = () =>{
+    const isDark = useColorScheme() == "dark";
+    
+    return (
+        <Tab.Navigator screenOptions={{
+            tabBarStyle:{
+                backgroundColor : isDark ? BLACK_COLOR : WHITE_COLOR
+            },
+            tabBarActiveTintColor:isDark?YELLOW_COLOR:BLACK_COLOR,
+            tabBarInactiveTintColor:isDark?GRAY_COLOR : DARK_GRAY_COLOR,
+            headerStyle:{
+                backgroundColor:isDark ? BLACK_COLOR : WHITE_COLOR,
+            },
+            headerTitleStyle:{
+                color:isDark?WHITE_COLOR:BLACK_COLOR,
+            }
+        }}>
 
-        {/* screenOptions={{
-            tabBarActiveTintColor:"red",
-            tabBarInactiveTintColor:"purple",
-            tabBarStyle: { backgroundColor:"tomato" },
-        }} */}
-        {/* options={{  
-            headerTitleStyle:{color:"tomato"},
-            headerRight:() => <View><Text>hello</Text></View>
-        }} */}
-        <Tab.Screen name="Movies" component={Movies} 
-        />
-        {/* options={{tabBarBadge:5}} */}
-        {/* tabBarActiveTintColor */}
-        <Tab.Screen name="Tv" component={Tv}  />
-        <Tab.Screen name="Search" component={Search}/>
-    </Tab.Navigator>
-)
+            {/* screenOptions={{
+                tabBarActiveTintColor:"red",
+                tabBarInactiveTintColor:"purple",
+                tabBarStyle: { backgroundColor:"tomato" },
+            }} */}
+            {/* options={{  
+                headerTitleStyle:{color:"tomato"},
+                headerRight:() => <View><Text>hello</Text></View>
+            }} */}
+
+            {/* options={{tabBarBadge:5}} */}
+            {/* tabBarActiveTintColor */}
+
+            <Tab.Screen name="Movies" component={Movies} />
+        
+            <Tab.Screen name="Tv" component={Tv}  />
+            <Tab.Screen name="Search" component={Search}/>
+        </Tab.Navigator>
+    )
+}
 
 
 export default Tabs;
