4c5c0009d1818a41790896861138bda2804fda4d
jungHyeonS <fbtls25@naver.com>
2022-10-09 14:00:52 +0900

    react-native : tab navigation config

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,14 +3,30 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-
+import { Text,View } from "react-native";
 const Tab = createBottomTabNavigator();
 
 
-const Tabs = () => <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies}/>
-    <Tab.Screen name="Tv" component={Tv}/>
-    <Tab.Screen name="Search" component={Search}/>
-</Tab.Navigator>
+const Tabs = () => (
+    <Tab.Navigator >
+
+        {/* screenOptions={{
+            tabBarActiveTintColor:"red",
+            tabBarInactiveTintColor:"purple",
+            tabBarStyle: { backgroundColor:"tomato" },
+        }} */}
+        {/* options={{  
+            headerTitleStyle:{color:"tomato"},
+            headerRight:() => <View><Text>hello</Text></View>
+        }} */}
+        <Tab.Screen name="Movies" component={Movies} 
+        />
+        {/* options={{tabBarBadge:5}} */}
+        {/* tabBarActiveTintColor */}
+        <Tab.Screen name="Tv" component={Tv}  />
+        <Tab.Screen name="Search" component={Search}/>
+    </Tab.Navigator>
+)
+
 
 export default Tabs;
