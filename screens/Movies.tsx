bc62423a3d0122f903d5410db8497ec524c566d8
jungHyeonS <fbtls25@naver.com>
2022-10-10 14:19:58 +0900

    react-native : react-web-swiper

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import {View,Text,TouchableOpacity,StyleSheet} from "react-native"
+import styled from 'styled-components/native' ;
+import {Text,TouchableOpacity,StyleSheet,Dimensions} from "react-native"
 import {  NativeStackScreenProps } from '@react-navigation/native-stack';
- 
+import Swiper from 'react-native-web-swiper';
 
-const Btn = styled.TouchableOpacity`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: ${(props) => props.theme.mainBgColor};
-`;
+// https://api.themoviedb.org/3/movie/now_playing?api_key=cec1dbc5f2281d2a147e666dc08b5e0f&language=en-US&page=1&region=KR
 
-const Title = styled.Text`
-    color: ${(props) => props.theme.textColor};
+const API_KEY = "cec1dbc5f2281d2a147e666dc08b5e0f";
+
+
+const Container = styled.ScrollView`
+    background-color: ${props => props.theme.mainBgClolor};
 `
 
-const Movies:React.FC<NativeStackScreenProps<any,'Movies'>> = ({navigation : {navigate}}) => (
-    <Btn
-    onPress={() => navigate("Stack",{screen:"Three"})}>
-        <Title>Movies</Title>
-    </Btn>
-)
+const View = styled.View`
+    flex: 1;
+`
+
+const {height : SCREEN_HEIGHT} = Dimensions.get("window");
+
+const Movies:React.FC<NativeStackScreenProps<any,'Movies'>> = () => {
+    const getNoewPlaying = () => {
+        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`);
+    }
+
+    return (
+        <Container>
+            <Swiper loop={true} timeout={3.5} controlsEnabled={false} containerStyle={{width:"100%",height:SCREEN_HEIGHT / 4}}>
+                <View style={{backgroundColor:"red"}}></View>
+                <View style={{backgroundColor:"blue"}}></View>
+                <View style={{backgroundColor:"red"}}></View>
+                <View style={{backgroundColor:"blue"}}></View>
+            </Swiper>
+        </Container>
+    )
+}
 
 
 
