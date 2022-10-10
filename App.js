4c52611ba61c2003af1f0e2c5101b731ab478fb6
jungHyeonS <fbtls25@naver.com>
2022-10-10 13:26:10 +0900

    react-native : theme

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { Image, Text,useColorScheme,View } from 'react-native';
 import * as Font from "expo-font"
 import {Ionicons} from "@expo/vector-icons"
 import { Asset, useAssets } from 'expo-asset';
-import { NavigationContainer,DarkTheme,DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer,DarkTheme,DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import Tabs from './navigation/Tabs';
 import Stack from './navigation/Stack';
 import Root from './navigation/Root';
+import { darkTheme, lightTheme } from './styled';
 
 
 // const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font))
@@ -24,16 +25,21 @@ export default function App() {
 
   const [assets] = useAssets([require("./images.png")])
   const [loaded] = Font.useFonts(Ionicons.font);
-  const isDark = useColorScheme() === "dark";
+  const isDark = useColorScheme() == "dark";
   if(!assets || !loaded){
     return (
       <AppLoading/>
      );
   }
+
+  
   return (
-     <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
         <Root/>
-    </NavigationContainer>
+      </NavigationContainer>
+    </ThemeProvider>
+    
   );
   
 }
