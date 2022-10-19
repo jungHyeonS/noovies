a049392395ec5e0261da3c96c987d81c5aa2a815
jungHyeonS <fbtls25@naver.com>
2022-10-19 22:44:19 +0900

    react-native : detail screen two

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import { Text, View,TouchableOpacity,Button, useColorScheme } from "react-native";
-import { YELLOW_COLOR,BLACK_COLOR,WHITE_COLOR } from "../colors";
+import { YELLOW_COLOR,BLACK_COLOR,WHITE_COLOR, GRAY_COLOR, DARK_GRAY_COLOR } from "../colors";
 import Detail from "../screens/Detail";
 
 
@@ -20,15 +20,30 @@ const NativeStack = createNativeStackNavigator();
 const Stack = () => {
     const isDark = useColorScheme() === "dark"
     return (
-        <NativeStack.Navigator screenOptions={{
-            headerBackTitleVisible:false,
+        <NativeStack.Navigator 
+        sceneContainerStyle={{
+            backgroundColor : isDark ? BLACK_COLOR : WHITE_COLOR
+        }}
+        screenOptions={{
+            unmountOnBlur:true,
+            tabBarStyle:{
+                backgroundColor : isDark ? BLACK_COLOR : WHITE_COLOR
+            },
+            tabBarActiveTintColor:isDark?YELLOW_COLOR:BLACK_COLOR,
+            tabBarInactiveTintColor:isDark?GRAY_COLOR : DARK_GRAY_COLOR,
             headerStyle:{
-                backgroundColor:isDark ? BLACK_COLOR:"white"
+                backgroundColor:isDark ? BLACK_COLOR : WHITE_COLOR,
             },
             headerTitleStyle:{
                 color:isDark?WHITE_COLOR:BLACK_COLOR,
             },
-        }}>
+            tabBarLabelStyle:{
+                marginTop:10,
+                fontSize:12,
+                fontWeight:"600"
+            }
+        }}
+        >
             <NativeStack.Screen name="Detail" component={Detail}/>
         </NativeStack.Navigator>
     )
--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,12 +1,47 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import styled from "styled-components/native";
 import { Movie, TV } from "../api";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../utils";
+import { BLACK_COLOR } from "../colors";
+
+const {height : SCREEN_HEIGHT} = Dimensions.get("window");
 
 const Container = styled.ScrollView`
     background-color: ${(props) => props.theme.mainBgColor};
+`
+
+
+const Header = styled.View`
+    height: ${SCREEN_HEIGHT / 4}px;
+    justify-content: flex-end;
+    padding:0px 20px;
+`;
+
+const Background = styled.Image`
+    
+`
+
+const Column = styled.View`
+    flex-direction: row;
+    width: 80%;
+`
+
+const Title = styled.Text`
+    color:white;
+    font-size: 26px;
+    align-self: flex-end;
+    
+    margin-left: 15px;
+    font-weight: 500;
+`
+const Overview = styled.Text`
+    color:#fff;
+    margin-top: 20px;
+    padding: 0px 20px;
 `
 
 type RootStackParamList = {
@@ -14,6 +49,7 @@ type RootStackParamList = {
 }
 
 type DetailScreenProps = NativeStackScreenProps<RootStackParamList,"Detail">
+
 
 const Detail:React.FC<DetailScreenProps> = (
     {
@@ -23,15 +59,25 @@ const Detail:React.FC<DetailScreenProps> = (
         }
     }
 ) => {
-    // console.log(params)
     useEffect(()=>{
         setOptions({
-            title : 'original_title' in params ? params.original_title : params.original_name
+            title : 'original_title' in params ? "Movie" : "Tv Show"
         })
     },[])
     return (
         <Container>
-            <Poster path={params.poster_path || ""}/>
+            <Header>
+                <Background style={StyleSheet.absoluteFill} source={{uri : makeImgPath(params.backdrop_path || "")}}/>
+                <LinearGradient colors={["transparent","#000"]} style={StyleSheet.absoluteFill}/>
+                <Column>
+                    <Poster path={params.poster_path || ""}/>
+                    <Title>
+                        {'original_title' in params ? params.original_title : params.original_name}
+                    </Title>
+                </Column>
+
+            </Header>
+            <Overview>{params.overview}</Overview>
         </Container>
     )
 }
